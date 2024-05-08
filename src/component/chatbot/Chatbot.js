06759753bf831f150c5615597aa556d5654afd9f
Daniel Lufupa <daniel.lufupa@hotmail.com>
2024-05-07 21:30:37 -0300

    mudamca de url email

--- src/component/chatbot/Chatbot.js
+++ src/component/chatbot/Chatbot.js
@@ -23,7 +23,7 @@ const Chatbot = () => {
       timeButton();
     } else {
       try {
-        await axios.post('https://gitalk-back-email-2.onrender.com/send-email', {
+        await axios.post('https://gitalk-back-email.onrender.com/send-email', {
           name,
           email,
           message,
